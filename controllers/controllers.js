017b3261b07beab84dbacdca012d726e61ab7d25
Harshit Sharma <harshitsharmabtp@gmail.com>
2021-01-09 17:39:24 +0530

    crud api's using async and await

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,8 @@
 const Blog=require('../model/model');
 
 //get all blogs
-exports.getall=(req,res)=>{
-     Blog.find({})
+exports.getall=async(req,res)=>{
+     /* Blog.find({})
      .then((Blogs) => {
         res.statusCode = 200;
         res.setHeader('Content-Type','application/json');
@@ -10,38 +10,68 @@ exports.getall=(req,res)=>{
     })
     .catch((err)=>{
         if(err) return res.status(500).json(err);
-    })
+    }) */
+     let blogs;
+     try {
+         blogs = await Blog.find({});
+         console.log(blogs);
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+    res.status(200).json(blogs);
 }
 
 //create a new blog
-exports.create=(req,res)=>{
+exports.create=async(req,res)=>{
     const newblog=new Blog({
         title:req.body.title,
         author:req.body.author,
         desc:req.body.desc
     });
-    newblog.save().then((blog)=>{
+    /* newblog.save().then((blog)=>{
         res.status(201).json({"msg":"created","blog":blog});
     }).catch((err)=>{
         if(err) return res.status(500).json(err);
-    })
+    }) */
+    let blog;
+     try {
+         blog = await newblog.save();
+         console.log(blog);
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+    res.status(200).json(blog);
 }
 
 //find single blog by id
-exports.getone=(req,res)=> {
-    Blog.findById(req.params.blogID)
+exports.getone=async(req,res)=> {
+    /* Blog.findById(req.params.blogID)
     .then((data)=>{
         if(!data) return res.status(404).json({"msg":"Blog Not Found"})
         res.status(200).json(data);
     })
     .catch((err)=>{
         if(err) res.status(500).json(err);
-    })
+    }) */
+    let blog;
+     try {
+         blog = await Blog.findById(req.params.blogID);
+         if(!blog)  return res.status(404).json({"msg":"Blog Not Found"})
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+    res.status(200).json(blog);
 }
 
 //find blog by author
-exports.getbyauthor=(req,res)=> {
-    Blog.find({author:req.params.author})
+exports.getbyauthor=async(req,res)=> {
+    /* Blog.find({author:req.params.author})
     .then((blog) => {
        if(blog.length===0) return res.status(404).json({"msg":"Blog with given author name Not Found"});
        res.statusCode = 200;
@@ -50,12 +80,24 @@ exports.getbyauthor=(req,res)=> {
    })
    .catch((err) => {
     if(err) res.status(500).json(err);
-   });
+   }); */
+   let blog;
+     try {
+         blog = await Blog.find({author:req.params.author});
+         if(blog.length===0) return res.status(404).json({"msg":"Blog with given author name Not Found"});
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+       res.statusCode = 200;
+       res.setHeader('Content-Type','application/json');
+       res.json(blog);
 }
 
 //find blog by title
-exports.getbytitle=(req,res)=>{
-    Blog.find({title:req.params.title})
+exports.getbytitle=async(req,res)=>{
+    /* Blog.find({title:req.params.title})
     .then((blog)=>{
         if(blog.length===0) return res.status(404).json({"msg":"Blog with given title Not Found"})
         res.statusCode=200;
@@ -63,12 +105,24 @@ exports.getbytitle=(req,res)=>{
     })
     .catch((err)=>{
         if(err) res.status(500).json(err);
-    })
+    }) */
+    let blog;
+     try {
+         blog = await Blog.find({title:req.params.title});
+         if(blog.length===0) return res.status(404).json({"msg":"Blog with given title Not Found"});
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+       res.statusCode = 200;
+       res.setHeader('Content-Type','application/json');
+       res.json(blog);
 }
 
 //find blog by description
-exports.getbydesc=(req,res)=>{
-    Blog.find({desc:req.params.desc})
+exports.getbydesc=async(req,res)=>{
+    /* Blog.find({desc:req.params.desc})
     .then((blog)=>{
         if(blog.length===0) return res.status(404).json({"msg":"Blog with given description Not Found"})
         res.statusCode=200;
@@ -76,16 +130,28 @@ exports.getbydesc=(req,res)=>{
     })
     .catch((err)=>{
         if(err) res.status(500).json(err);
-    })
+    }) */
+    let blog;
+     try {
+         blog = await Blog.find({desc:req.params.desc});
+         if(blog.length===0) return res.status(404).json({"msg":"Blog with given description Not Found"});
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+       res.statusCode = 200;
+       res.setHeader('Content-Type','application/json');
+       res.json(blog);
 }
 
 
 //to update a blog
-exports.updateone=(req,res)=>{
+exports.updateone=async(req,res)=>{
     if(!req.body.title||!req.body.author||!req.body.desc)
       return res.status(500).json({"msg":"fill all the fields"});
     
-    Blog.findByIdAndUpdate(req.params.blogID,{
+    /* Blog.findByIdAndUpdate(req.params.blogID,{
         title: req.body.title,
         //author: req.body.author,
         desc: req.body.desc 
@@ -99,13 +165,30 @@ exports.updateone=(req,res)=>{
     })
     .catch((err)=>{
         if(err) res.status(500).json(err);
-    })
+    }) */
+    let blog;
+     try {
+         blog = await Blog.findByIdAndUpdate(req.params.blogID,{
+            title: req.body.title,
+            //author: req.body.author,
+            desc: req.body.desc},{new: true});
+
+           if(!blog) return res.status(404).json({"msg":"not found"});
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+    res.status(202).json({
+        "msg":"updated",
+        "doc":blog
+    });
 }
 
 //to delete a blog
 
-exports.deleteone=(req,res)=>{
-    Blog.findByIdAndDelete(req.params.blogID)
+exports.deleteone=async(req,res)=>{
+    /* Blog.findByIdAndDelete(req.params.blogID)
     .then((data)=>{
         if(!data) return res.status(404).json({"msg":"Blog not found"});
 
@@ -116,5 +199,19 @@ exports.deleteone=(req,res)=>{
     })
     .catch((err)=>{
         if(err) res.status(500).json(err);
-    })
+    }) */
+    let blog;
+     try {
+         blog = await Blog.findByIdAndDelete(req.params.blogID);
+
+           if(!blog) return res.status(404).json({"msg":"Blog not found"});
+         } 
+    catch (err) 
+    {
+        if(err) return res.status(500).json(err);
+    }
+    res.status(202).json({
+        "msg":"deleted",
+        "doc":blog
+    });
 }
